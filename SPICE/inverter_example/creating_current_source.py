f14c7607aef979726b794a74572db67efc602084
Daniel Limbrick <dblimbrick@gmail.com>
2026-02-25 14:03:26 -0500

    Update creating_current_source.py

--- SPICE/inverter_example/creating_current_source.py
+++ SPICE/inverter_example/creating_current_source.py
@@ -11,11 +11,11 @@ t = np.linspace(0, 1e-9, 200)
 I = I0*(np.exp(-t/tau_f) - np.exp(-t/tau_r))
 
 # --- Plot ---
-plt.plot(t*1e12, I*1e6)
-plt.xlabel("Time (ps)")
-plt.ylabel("Current (uA)")
-plt.title("SET Double Exponential Current")
-plt.show()
+# plt.plot(t*1e12, I*1e6)
+# plt.xlabel("Time (ps)")
+# plt.ylabel("Current (uA)")
+# plt.title("SET Double Exponential Current")
+# plt.show()
 
 
 with open("set_pwl.sp", "w") as f:
